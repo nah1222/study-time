2317be09761d560075743cbc391036b7a397188b
nah1222 <nah1222@yahoo.com>
2021-04-13 19:22:45 -0400

    middle

--- index.js
+++ index.js
@@ -79,3 +79,15 @@ const runningSum = function(nums) {
     return nums
     total = 0
 };
+
+function getMiddle(s){
+  //Code goes here!
+  let length = s.length;
+  let middle = Math.floor(length/2);
+  
+  if (length%2 === 0){
+    return s[middle - 1] + s[middle]
+  }else{
+    return s[middle]
+  }
+}
