9457ba77f77e4915476cabeb4d85828201bf9406
nah1222 <nah1222@yahoo.com>
2021-04-21 16:16:24 -0400

    seriesSum

--- index.js
+++ index.js
@@ -91,3 +91,13 @@ function getMiddle(s){
     return s[middle]
   }
 }
+
+function SeriesSum(n)
+{
+  let sum = 0;
+  for( let i = 0; i < n; i++){
+    sum += 1/(i*3 + 1);
+  }
+  return sum.toFixed(2);
+}
+
