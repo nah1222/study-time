00e9375fa5c611a1cea79b8a32829c6fbc3551c3
nah1222 <nah1222@yahoo.com>
2021-04-22 13:50:18 -0400

    frequency counter naive

--- index.js
+++ index.js
@@ -101,3 +101,18 @@ function SeriesSum(n)
   return sum.toFixed(2);
 }
 
+//write a function called same, which accepts two arrays. the function should return true if every value in the second array. the frequency must be the same.
+//naive solution time complexity n^2
+function same(arr1, arr2){
+  if (arr1.length !== arr2.length){
+    return false;
+  }
+  for(let i = 0; i < arr1.length; i++){
+    let correctIndex = arr2.indexOf(arr1[i] ** 2)
+    if(correctIndex === -1) {
+      return false;
+    }
+    arr2.splice(correctIndex, 1)
+  }
+  return true
+}
