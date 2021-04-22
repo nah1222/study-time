a3db625769c570e4e7927826d96fe5aabb1f664b
nah1222 <nah1222@yahoo.com>
2021-04-22 14:40:11 -0400

    multiple pointers pattern

--- index.js
+++ index.js
@@ -162,3 +162,15 @@ function validAnagram(first, second){
   }
   return true;
 }
+
+//write a function called sumZero which accepts a sorted array of integers. the function should find the first pair where a sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist
+//naive solution time complexity O(N^2) space complexity - O(1)
+function sumZero(arr){
+  for(let i = 0; i < arr.length; i++){
+    for(let j = i+1; j < arr.length; j++){
+      if(arr[i] + arr[j] === 0){
+        return[arr[i], arr[j]]
+      }
+    }
+  }
+}
