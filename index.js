25afc4898cdc9b593c0cf79efb901a5b46c2d14c
nah1222 <nah1222@yahoo.com>
2021-04-22 14:30:31 -0400

    anagram

--- index.js
+++ index.js
@@ -139,3 +139,26 @@ function same(arr1, arr2){
   }
   return true
 }
+
+//Given two strings, write a function to determine if the second string is an anagram of the first.
+function validAnagram(first, second){
+  if(first.length !== second.length){
+    return false;
+  }
+  const lookup = {};
+
+  for(let i = 0; i < first.length; i++){
+    let letter = first[i];
+    //if letter exists, increment, otherwise set to 1
+    lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1
+  }
+  for(let i = 0; i < second.length; i++){
+    let letter = second[i];
+    if (!lookup[letter]){
+      return false;
+    }else{
+      lookup[letter] -= 1;
+    }
+  }
+  return true;
+}
